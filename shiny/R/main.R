986fe5472aad3094363419d22bdf84d99b53624b
blaquans <pachevalier@gmail.com>
2014-07-11 16:13:29 +0200

    new functions

---
+++ shiny/R/main.R
@@ -0,0 +1,6 @@
+# devtools::install_github("rstudio/shinyapps")
+
+shinyapps::setAccountInfo(name='blaquans', token='92DECD13F0A302ADA0D6F5B700D8675A', secret='s7ivlrsUnWG7yy/k56g0hgcI0LS0WLPbXoyloUDj')
+
+library(shiny)
+runApp("shiny/R")
